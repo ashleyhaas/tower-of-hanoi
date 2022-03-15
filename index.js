b1a067bef9d5a5f70464b3076e26c505b8230cfe
Ashley Haas <ashley@avironsoftware.com>
2022-03-15 16:01:15 -0500

    final game

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // respond to clicks , boxes different sizes
 let tower1 = document.querySelector("#tower-1");
+let selectedDisc;
 tower1.addEventListener("click", function() {
-    let topDisc = tower1.children[tower1.children.length-1];
-    topDisc.style.border = "1px black dashed"
+    topDisc = tower1.children[tower1.children.length-1];
+    if (selectedDisc) {
+        if (topDisc  === undefined || topDisc.offsetWidth > selectedDisc.offsetWidth) {
+            tower1.appendChild(selectedDisc);
+            selectedDisc.style.border = ""
+            selectedDisc = undefined;
+        }
+    } else if (topDisc) {
+            selectedDisc = topDisc;
+            selectedDisc.style.border = "1px black dashed";
+    }
 })
+
 let discElement = document.createElement("div");
 discElement.style.height = "20px";
 discElement.style.width = "150px";
@@ -27,3 +38,40 @@ discElement4.style.height = "20px";
 discElement4.style.width = "90px";
 discElement4.style.backgroundColor = "yellow";
 tower1.appendChild(discElement4);
+
+let tower2 = document.querySelector("#tower-2");
+tower2.addEventListener("click", function() {
+    topDisc2 = tower2.children[tower2.children.length-1];
+    if (selectedDisc) {
+        if (topDisc2  === undefined || topDisc2.offsetWidth > selectedDisc.offsetWidth) {
+            tower2.appendChild(selectedDisc);
+            selectedDisc.style.border = ""
+            selectedDisc = undefined;
+        }
+    } else if (topDisc2) {
+            selectedDisc = topDisc2;
+            selectedDisc.style.border = "1px black dashed";
+    }
+})
+
+let tower3 = document.querySelector("#tower-3");
+tower3.addEventListener("click", function() {
+    topDisc3 = tower3.children[tower3.children.length-1];
+    if (selectedDisc) {
+        if (topDisc3  === undefined || topDisc3.offsetWidth > selectedDisc.offsetWidth) {
+            tower3.appendChild(selectedDisc);
+            selectedDisc.style.border = ""
+            selectedDisc = undefined;
+        }
+    } else if (topDisc3) {
+            selectedDisc = topDisc3;
+            selectedDisc.style.border = "1px black dashed";
+    }
+    if (tower3.children.length === 4) {
+        alert("You won!");
+    }
+})
+
+
+//Check width of top disc against width of selected disc
+//If top disc is wider than selected disc, we can't place it there, else place it there
